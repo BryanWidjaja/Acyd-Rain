ca117b765088db2c03d69bff59d77b2ba18bb89f
Bryan Widjaja <crz.github@gmail.com>
2025-08-19 19:33:36 +0700

    minor update

--- components/themeSwitch.js
+++ components/themeSwitch.js
@@ -1,27 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const darkmodeBtn = document.getElementById("darkmode-btn");
-  const themeIcon = document.querySelector(".theme-icon");
+const themeBtn = document.getElementById("theme-btn");
+const themeIcon = document.querySelector(".theme-icon");
 
-  const dark = "./assets/icons/dark.svg";
-  const light = "./assets/icons/light.svg";
+const dark = "./assets/icons/dark.svg";
+const light = "./assets/icons/light.svg";
 
-  let currThemeSetting = localStorage.getItem("theme") || "dark";
-  document.documentElement.setAttribute("data-theme", currThemeSetting);
+let currThemeSetting = localStorage.getItem("theme") || "dark";
+document.documentElement.setAttribute("data-theme", currThemeSetting);
 
-  if (themeIcon) {
-    themeIcon.src = currThemeSetting === "dark" ? light : dark;
-  }
+if (themeIcon) {
+  themeIcon.src = currThemeSetting === "dark" ? light : dark;
+}
 
-  if (darkmodeBtn) {
-    darkmodeBtn.addEventListener("click", () => {
-      const newTheme = currThemeSetting === "dark" ? "light" : "dark";
-      document.documentElement.setAttribute("data-theme", newTheme);
-      localStorage.setItem("theme", newTheme);
-      currThemeSetting = newTheme;
+if (themeBtn) {
+  themeBtn.addEventListener("click", () => {
+    const newTheme = currThemeSetting === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+    currThemeSetting = newTheme;
 
-      if (themeIcon) {
-        themeIcon.src = newTheme === "dark" ? light : dark;
-      }
-    });
-  }
-});
+    if (themeIcon) {
+      themeIcon.src = newTheme === "dark" ? light : dark;
+    }
+  });
+}
